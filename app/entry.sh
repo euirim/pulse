49ffc9be75b9f37531ecc1da1011781fb1137153
Euirim Choi <euirim@me.com>
2019-01-22 21:21:24 -0600

    Fix entry script migrate and add keyphrases commands

--- app/entry.sh
+++ app/entry.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Apply migrations
+pipenv run python manage.py migrate
+
 # Add/update keyphrases (does no delete)
-python manage.py add_keyphrases data/2020_national_candidates.json
+pipenv run python manage.py add_keyphrases data/2020_national_candidates.json
 
 # start cron
 /usr/sbin/crond -f -l 8
