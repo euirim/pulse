3d058a749e7a3e99cb456cd2f9b2ab3942bef1a9
Euirim Choi <euirim@me.com>
2019-04-01 19:53:50 -0500

    Add euirim.org as allowed host

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '67g$0ouvkr#+#5r=sst-b78kt=tb9q6sb^3!*wp83v_=ah*zzz'
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'euirim.org']
 
 
 # Admins
