196dc5bae5502fea26ccf9dcbab61bc29574f105
Euirim Choi <euirim@me.com>
2019-04-02 17:35:59 -0500

    Add additional allowed_host for semantic url

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = '67g$0ouvkr#+#5r=sst-b78kt=tb9q6sb^3!*wp83v_=ah*zzz'
 ALLOWED_HOSTS = [
     'localhost', 
     'euirim.org', 
-    'projects.euirim.org.s3-website.us-east-2.amazonaws.com',
+    'pulse.euirim.org',
 ]
 
 
